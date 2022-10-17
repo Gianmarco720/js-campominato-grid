3152e68d64ba903540045273a57ef45c520ff64e
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-17 18:02:33 +0200

    add numbers to cells

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,23 +6,26 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro
 ed emetto un messaggio in console con il numero della cella cliccata.
 */
 
-// seleziono il container e il bottone con queryselector
+// seleziono gli elementi della DOM con queryselector
 const container = document.querySelector('.my_container');
 const gameBtn = document.querySelector('.start_game');
 
-// creo l'elemento da inserire nella DOM
-let cells = `<div class="cell"></div>`;
+// dichiaro una variabile per il numero delle celle
 let cellsNum = 100;
 
 // aggiungo un eventlistener per creare la griglia al click del bottone
 gameBtn.addEventListener('click', function() {
     // invoco la funzione per la griglia
     gridGen(100, container);
+    
+    
 })
 
-// creo una funzione per generare le griglia
+// FUNCTIONS
+
+// funzione per generare le griglia
 function gridGen(num, domEl) {
-    for (let i = 0; i < num; i++) {
-        domEl.innerHTML += cells;
+    for (let i = 1; i <= num; i++) {
+        domEl.innerHTML += `<div class="cell">${i}</div>`;
     }
 }

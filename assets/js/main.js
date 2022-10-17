cd2a5e99fb074badc626da069d78708d51b9d89e
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-17 15:39:19 +0200

    add grid on button click

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/* 
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+ed emetto un messaggio in console con il numero della cella cliccata.
+*/
+
+// seleziono il container e il bottone con queryselector
+const container = document.querySelector('.container');
+const gameBtn = document.querySelector('.start_game');
+
+// creo l'elemento da inserire nella DOM
+let cells = `<div class="cell"></div>`;
+let cellsNum = 100;
+
+// aggiungo un eventlistener per creare la griglia al click del bottone
+gameBtn.addEventListener('click', function() {
+    // creo la griglia con un ciclo for
+    for (let i = 0; i < cellsNum; i++) {
+        container.innerHTML += cells;
+    }
+})
